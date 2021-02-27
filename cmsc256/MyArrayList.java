f5105bdb88d7239ad75cd6f477ceeebe65e3fd56
Debra M. Duke <debra.duke@gmail.com>
2021-02-27 12:37:04 -0500

    Update MyArrayList.java
    rollback

--- cmsc256/MyArrayList.java
+++ cmsc256/MyArrayList.java
@@ -97,7 +97,7 @@ public class MyArrayList<T> implements List<T> {
 	 * @param target
 	 * @param object
 	 */
-	private boolean equals(T target, T element) {
+	private boolean equals(Object target, Object element) {
 		if (target == null) {
 			return element == null;
 		} else {
